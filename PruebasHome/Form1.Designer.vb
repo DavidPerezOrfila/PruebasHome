bb5fe174b8b9b960436907eceb2e0cddb554acb9
David Pérez Orfila <knight_bcn@hotmail.com>
2016-02-06 16:47:01 +0100

    Repaso

--- PruebasHome/Form1.Designer.vb
+++ PruebasHome/Form1.Designer.vb
@@ -23,22 +23,66 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
         Me.Button1.Location = New System.Drawing.Point(0, 12)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(114, 23)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Ex1"
+        Me.Button1.Text = "Ex1 EstCtrl"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(0, 41)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(114, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Ex2 EstCtrl"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(0, 70)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(114, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Ex3 EstCtrl"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(0, 99)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(114, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Ex4 EstCtrl"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(0, 128)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(114, 23)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Ex5 EstCtrl"
+        Me.Button5.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.ClientSize = New System.Drawing.Size(426, 546)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Ex1"
@@ -47,4 +91,8 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
 End Class
--- PruebasHome/Form1.vb
+++ PruebasHome/Form1.vb
@@ -1,5 +1,95 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Programa que calcula àrea i perímetre d'un cercle
+        Const PI = 3.141592 'Definició de la constant PI 
+        Dim radi, area, perimetre As Double
+        Dim salida As String = ""
+        radi = InputBox("Introdueix el radi del cercle:")
+        If radi > 0 Then 'que el radi sigui positiu'
+            area = PI * radi * radi
+            perimetre = 2 * PI * radi
+            salida += "L'area del cercle és: " + area.ToString + vbNewLine +
+            "El perimetre de la circumferencia és: " + perimetre.ToString + vbNewLine
+        End If
+        MessageBox.Show(salida)
 
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Programa que calcula àrea i perímetre d'un cercle
+        Const PI = 3.141592 'Definició de la constant PI 
+        Dim radi, area, perimetre As Double
+        Dim salida As String = ""
+        radi = InputBox("Introdueix el radi del cercle:")
+        If radi > 0 Then 'que el radi sigui positiu'
+            area = PI * radi * radi
+            perimetre = 2 * PI * radi
+            salida += "L'area del cercle és: " + area.ToString + vbNewLine +
+            "El perimetre de la circumferencia és: " + perimetre.ToString + vbNewLine
+        Else 'Error si el valor del radi és negatiu'
+            salida += "El valor ha de ser positiu"
+        End If
+        MessageBox.Show(salida)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Programa que simula el funcionament d'una calculadora simple'
+        Dim operacio As Char 'Operació que s'ha de realitzar.'
+        Dim x, y As Integer 'Valors d'entrada. */
+        x = InputBox("Introdueix un número: ")
+        y = InputBox("Introdueix un número: ")
+        operacio = InputBox("Introdueix una operació vàlida: ")
+
+        Select Case operacio
+
+            Case "+"
+                MessageBox.Show(x + y)
+            Case "-"
+                MessageBox.Show(x - y)
+
+            Case "*"
+                MessageBox.Show(x * y)
+
+            Case "/"
+                MessageBox.Show(x / y)
+            Case Else
+                MessageBox.Show("Operació incorrecta.")
+        End Select
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'Programa que calcula el factorial d'un nombre enter amb bucle while'
+        Dim n, factorial, i As Integer
+        Dim salida As String = ""
+
+        n = InputBox("Introdueix un nombre positiu:")
+        factorial = 1 'Inicialització variables bucle'
+        i = 1
+        While i < n 'Condició de finalització
+
+            i = i + 1 'Actualització variables bucle'
+            factorial = factorial * i ' Accions del bucle'
+
+            salida += "El factorial és: " + factorial.ToString + vbNewLine
+
+        End While
+        MessageBox.Show(salida)
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'Programa que calcula el factorial d'un nombre enter bucle for
+        Dim n, factorial, i As Integer
+        Dim salida As String = ""
+        n = InputBox("Introdueix un numero positiu:")
+        factorial = 1 'Inicialització, actualització var control, condició final dins del for
+        For i = 2 To i <= n
+            factorial = factorial * i 'Accions del bucle'
+            salida += "El factorial és: " + factorial.ToString
+        Next
+        MessageBox.Show(salida)
     End Sub
 End Class
